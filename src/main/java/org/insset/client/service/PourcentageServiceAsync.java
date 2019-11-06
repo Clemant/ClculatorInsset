1d32215539a58efe4e2bc4e6d693bb2b3b1b3244
Clement97712 <mj.capou@gmail.com>
2019-11-06 06:04:39 -0400

    create the different file for 'Pourcentage'

---
+++ src/main/java/org/insset/client/service/PourcentageServiceAsync.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.insset.client.service;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.RemoteService;
+
+
+/**
+ *
+ * @author victoire
+ */
+public interface PourcentageServiceAsync {
+    void pourcentage( double prix, double remise, AsyncCallback<String> callback);
+    
+}
---
+++ src/main/java/org/insset/server/PourcentageServiceImpl.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.insset.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import org.insset.client.service.PourcentageService;
+
+/**
+ *
+ * @author victoire
+ */
+public class PourcentageServiceImpl extends RemoteServiceServlet implements
+        PourcentageService {
+
+    @Override
+    public Double pourcentage(double prix, double remise,)  {
+         //To change body of generated methods, choose Tools | Templates.
+       
+    }
+    
+    
+}
