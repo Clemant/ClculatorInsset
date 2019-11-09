65b235a59396782375e98f0726ac5048f2888cc7
Clement97712 <mj.capou@gmail.com>
2019-11-09 14:47:26 -0400

    Create the Unit test for RomanConverterServiceImpl class

--- src/test/java/org/insset/server/RomanConverterServiceImplTest.java
+++ src/test/java/org/insset/server/RomanConverterServiceImplTest.java
@@ -30,35 +30,17 @@ public class RomanConverterServiceImplTest {
 
     @Test
     public void testConvertDateYears() {
-        System.out.println("convertDateYears");
-        String nbr = "";
-        RomanConverterServiceImpl instance = new RomanConverterServiceImpl();
-        String expResult = "";
-        String result = instance.convertDateYears(nbr);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        
     }
 
     @Test
     public void testConvertRomanToArabe() {
-        System.out.println("convertRomanToArabe");
-        String nbr = "";
-        RomanConverterServiceImpl instance = new RomanConverterServiceImpl();
-        Integer expResult = null;
-        Integer result = instance.convertRomanToArabe(nbr);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        
     }
 
     @Test
     public void testConvertArabeToRoman() {
-        System.out.println("convertArabeToRoman");
-        Integer nbr = null;
-        RomanConverterServiceImpl instance = new RomanConverterServiceImpl();
-        String expResult = "";
-        String result = instance.convertArabeToRoman(nbr);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+       
     }
     
 }
